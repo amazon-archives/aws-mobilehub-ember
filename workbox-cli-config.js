12188fcc809913095db0511528b6d9b8e5467bfd
Michael Labieniec <michaellabieniec@gmail.com>
2017-10-14 19:02:17 -0400

    add pwa with workbox config

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{css,js,map,png,xml,gif,html,txt}"
+  ],
+  "swDest": "./public/sw.js",
+  "globIgnores": [
+    "../workbox-cli-config.js"
+  ]
+};
